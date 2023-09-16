2c38669f6192e2380539bac4d240c1790347ae2d
mrcloud <mister.cloud@yandex.ru>
2023-09-16 21:09:47 +0300

    feat: решение 1720

--- 1720/main.go
+++ 1720/main.go
@@ -8,10 +8,9 @@ func main() {
 	fmt.Println(x, ` - `, decode(x, 1), ` expected `, []int{1, 0, 2, 1})
 	x = []int{6, 2, 7, 3}
 	fmt.Println(x, ` - `, decode(x, 4), ` expected `, []int{4, 2, 0, 7, 4})
-	x = []int{}
-	fmt.Println(x, ` - `, decode(x, 4), ` expected `, []int{4, 2, 0, 7, 4})
 }
 
+// https://leetcode.com/problems/decode-xored-array/submissions/1051156050/
 func decode(encoded []int, first int) []int {
 	var decoded = make([]int, len(encoded)+1)
 	decoded[0] = first
@@ -20,12 +19,4 @@ func decode(encoded []int, first int) []int {
 	}
 	return decoded
 
-}
-
-func encode(decoded []int) []int {
-	var encoded []int
-	for i := 0; i < len(decoded)-1; i++ {
-		encoded = append(encoded, decoded[i]^decoded[i+1])
-	}
-	return encoded
 }
